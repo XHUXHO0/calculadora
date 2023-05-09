3a26b374e4b7d73c6dcc207fa02a9933a1234e24
Jesus Sanchez Sarmiento <xhuxho.emprende@gmail.com>
2023-05-08 23:18:15 -0600

    se colocaron los botones de manera gloval

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -14,36 +14,84 @@ class MainActivity : AppCompatActivity() {
     private var resultado: Double = 0.0
     private var operacion: Int = 0
 
+    private lateinit var btn_0: Button
+    private lateinit var btn_1: Button
+    private lateinit var btn_2: Button
+    private lateinit var btn_3: Button
+    private lateinit var btn_4: Button
+    private lateinit var btn_5: Button
+    private lateinit var btn_6: Button
+    private lateinit var btn_7: Button
+    private lateinit var btn_8: Button
+    private lateinit var btn_9: Button
+    private lateinit var btn_punto: Button
+
+    private lateinit var btn_suma: Button
+    private lateinit var btn_resta: Button
+    private lateinit var btn_por: Button
+    private lateinit var btn_divicion: Button
+    private lateinit var btn_gramaje: Button
+
+    private lateinit var btn_limpiar: Button
+    private lateinit var btn_ver_lista: Button
+    private lateinit var btn_agregar: Button
+    private lateinit var btn_igual: Button
+    private lateinit var txt_resultado: TextView
+    private lateinit var txt_historial: TextView
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.btn_0).setOnClickListener{numeroPresionado("0")}
-        findViewById<Button>(R.id.btn_1).setOnClickListener{numeroPresionado("1")}
-        findViewById<Button>(R.id.btn_2).setOnClickListener{numeroPresionado("2")}
-        findViewById<Button>(R.id.btn_3).setOnClickListener{numeroPresionado("3")}
-        findViewById<Button>(R.id.btn_4).setOnClickListener{numeroPresionado("4")}
-        findViewById<Button>(R.id.btn_5).setOnClickListener{numeroPresionado("5")}
-        findViewById<Button>(R.id.btn_6).setOnClickListener{numeroPresionado("6")}
-        findViewById<Button>(R.id.btn_7).setOnClickListener{numeroPresionado("7")}
-        findViewById<Button>(R.id.btn_8).setOnClickListener{numeroPresionado("8")}
-        findViewById<Button>(R.id.btn_9).setOnClickListener{numeroPresionado("9")}
-        findViewById<Button>(R.id.btn_punto).setOnClickListener{numeroPresionado(".")}
+        btn_0 = findViewById(R.id.btn_0)
+        btn_1 = findViewById(R.id.btn_1)
+        btn_2 = findViewById(R.id.btn_2)
+        btn_3 = findViewById(R.id.btn_3)
+        btn_4 = findViewById(R.id.btn_4)
+        btn_5 = findViewById(R.id.btn_5)
+        btn_6 = findViewById(R.id.btn_6)
+        btn_7 = findViewById(R.id.btn_7)
+        btn_8 = findViewById(R.id.btn_8)
+        btn_9 = findViewById(R.id.btn_9)
+        btn_punto = findViewById(R.id.btn_punto)
 
-        findViewById<Button>(R.id.btn_suma).setOnClickListener{operacionPresionado(SUMA)}
-        findViewById<Button>(R.id.btn_resta).setOnClickListener{operacionPresionado(RESTA)}
-        findViewById<Button>(R.id.btn_por).setOnClickListener{operacionPresionado(MULTIPLICACION)}
-        findViewById<Button>(R.id.btn_divicion).setOnClickListener{operacionPresionado(DIVISION)}
-        findViewById<Button>(R.id.btn_gramaje).setOnClickListener{operacionPresionado(GRAMAJE)}
+        btn_suma = findViewById<Button>(R.id.btn_suma)
+        btn_resta = findViewById<Button>(R.id.btn_resta)
+        btn_por = findViewById<Button>(R.id.btn_por)
+        btn_divicion = findViewById<Button>(R.id.btn_divicion)
+        btn_gramaje = findViewById<Button>(R.id.btn_gramaje)
 
-        findViewById<Button>(R.id.btn_limpiar).setOnClickListener{ resetAll()}
+        btn_limpiar = findViewById<Button>(R.id.btn_limpiar)
+        btn_ver_lista = findViewById<Button>(R.id.btn_ver_lista)
+        btn_agregar = findViewById<Button>(R.id.btn_agregar)
+        btn_igual = findViewById<Button>(R.id.btn_igual)
+        txt_resultado =findViewById<TextView>(R.id.tv_resultado)
+        txt_historial =findViewById<TextView>(R.id.tv_historial)
 
-        findViewById<Button>(R.id.btn_ver_lista).setOnClickListener{ resetCalculadora()}
+        btn_0.setOnClickListener{numeroPresionado("0")}
+        btn_1.setOnClickListener{numeroPresionado("1")}
+        btn_2.setOnClickListener{numeroPresionado("2")}
+        btn_3.setOnClickListener{numeroPresionado("3")}
+        btn_4.setOnClickListener{numeroPresionado("4")}
+        btn_5.setOnClickListener{numeroPresionado("5")}
+        btn_6.setOnClickListener{numeroPresionado("6")}
+        btn_7.setOnClickListener{numeroPresionado("7")}
+        btn_8.setOnClickListener{numeroPresionado("8")}
+        btn_9.setOnClickListener{numeroPresionado("9")}
+        btn_punto.setOnClickListener{numeroPresionado(".")}
 
+        btn_suma.setOnClickListener{operacionPresionado(SUMA)}
+        btn_resta.setOnClickListener{operacionPresionado(RESTA)}
+        btn_por.setOnClickListener{operacionPresionado(MULTIPLICACION)}
+        btn_divicion.setOnClickListener{operacionPresionado(DIVISION)}
+        btn_gramaje.setOnClickListener{operacionPresionado(GRAMAJE)}
 
-        findViewById<Button>(R.id.btn_agregar).setOnClickListener{ guadar() }
-
-        findViewById<Button>(R.id.btn_igual).setOnClickListener{ resolvePressed() }
+        btn_limpiar.setOnClickListener{ resetAll()}
+        btn_ver_lista.setOnClickListener{ resetCalculadora()}
+        btn_agregar.setOnClickListener{ guadar() }
+        btn_igual.setOnClickListener{ resolvePressed() }
 
 
 
@@ -53,31 +101,28 @@ class MainActivity : AppCompatActivity() {
 
 
 
-            if (findViewById<TextView>(R.id.tv_resultado).text == "0" && digito != ".") {
-                findViewById<TextView>(R.id.tv_resultado).text = "$digito"
+            if (txt_resultado.text == "0" && digito != ".") {
+                txt_resultado.text = digito
             } else {
-                if("$digito" == "."){
-                    findViewById<TextView>(R.id.tv_resultado).text = "0."
+                if(digito == "." && txt_resultado.text == "0.0"){
+                    txt_resultado.text = "0."
                 }else{
-                    findViewById<TextView>(R.id.tv_resultado).text =
-                        "${findViewById<TextView>(R.id.tv_resultado).text}$digito"
+                    txt_resultado.text = "${txt_resultado.text}$digito"
                 }
             }
 
             if (operacion == NO_HAY_OPERACION) {
-                num1 = findViewById<TextView>(R.id.tv_resultado).text.toString().toDouble()
+                num1 = txt_resultado.text.toString().toDouble()
             } else {
-                num2 = findViewById<TextView>(R.id.tv_resultado).text.toString().toDouble()
+                num2 = txt_resultado.text.toString().toDouble()
             }
 
 
     }
 
     private fun operacionPresionado(operacion: Int){
-
         this.operacion = operacion
-
-        findViewById<TextView>(R.id.tv_resultado).text = "0"
+        txt_resultado.text = "0"
     }
 
     companion object{
@@ -90,7 +135,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun resolvePressed(){
-        if(num1 != 0.0 ){
+        if(num1 != 0.0 && operacion != 0){
             val result = when(operacion) {
                 SUMA -> num1 + num2
                 RESTA -> num1 - num2
@@ -104,9 +149,9 @@ class MainActivity : AppCompatActivity() {
             num1 = result as Double
 
 
-            var resultados = if("$result".endsWith(".0")) { "$result".replace(".0","") } else { "%.2f".format(result) }
+            val resultados = if("$result".endsWith(".0")) { "$result".replace(".0","") } else { "%.2f".format(result) }
 
-            findViewById<TextView>(R.id.tv_resultado).text = resultados
+            txt_resultado.text = resultados
         }else{
             val toast = Toast.makeText(this, "Error no hay nada para datos para calcular", Toast.LENGTH_LONG)
             toast.show()
@@ -117,15 +162,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun resetAll(){
-        findViewById<TextView>(R.id.tv_resultado).text = "0"
+        txt_resultado.text = "0"
         num1 = 0.0
         num2 = 0.0
         operacion = NO_HAY_OPERACION
     }
 
     private fun resetCalculadora(){
-        findViewById<TextView>(R.id.tv_resultado).text = "0"
-        findViewById<TextView>(R.id.tv_historial).text = ""
+        txt_resultado.text = "0"
+        txt_historial.text = ""
         num1 = 0.0
         num2 = 0.0
         num3 = 0.0
@@ -135,12 +180,12 @@ class MainActivity : AppCompatActivity() {
 
     private fun guadar(){
         if(resultado != 0.0){
-            num3 = resultado.toString().toDouble() + findViewById<TextView>(R.id.tv_resultado).text.toString().toDouble()
+            num3 = resultado.toString().toDouble() + txt_resultado.text.toString().toDouble()
             resultado = num3
         }else{
-            resultado = findViewById<TextView>(R.id.tv_resultado).text.toString().toDouble()
+            resultado = txt_resultado.text.toString().toDouble()
         }
-        findViewById<TextView>(R.id.tv_historial).text = resultado.toString()
+        txt_historial.text = resultado.toString()
         resetAll()
     }
 
